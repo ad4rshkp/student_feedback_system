6c2247b37f242d76a3c7951f2b7a2e441fb9a2ed
ad4rshkp <kpadarsh123@gmail.com>
2025-08-05 22:23:50 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+
+C:\tc_project\student_feedback_system>git commit -m "Initial commit"
+On branch main
+
+Initial commit
+
+nothing to commit (create/copy files and use "git add" to track)
+
+C:\tc_project\student_feedback_system>
